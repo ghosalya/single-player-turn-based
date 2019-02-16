35709ec6a2510cdd1bcd5d67f3d5f99107ef40b2
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-02-16 16:54:33 +0800

    Added base script for Cards and Effects

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Card")]
+public class Card : ScriptableObject
+{
+    public enum CardType
+    {
+        ATTACK,
+        DEFENSE,
+        POWER,
+        ULTIMATE
+    }
+
+    public string cardName;
+    public string description;
+    public int cost;
+    public CardType type;
+    public Effect[] effects;
+}
