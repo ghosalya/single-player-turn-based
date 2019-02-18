bab9ea6fcb63e10d8ad36b32cc2b5ed615646dc6
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-02-18 23:01:10 +0800

    Added floating healthbar on enemies

---
+++ Assets/Scripts/UI/UnitHealthbar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealthbar : MonoBehaviour
+{
+    public static float XScale = 0.15f;
+    public static float ZScale = 0.0075f;
+    public Transform healthbarPlane;
+    public TextMesh healthbarNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateHealth();
+        // healthbarPlane.LookAt(Camera.main.transform);
+    }
+
+    void updateHealth()
+    {
+        UnitHealth unitHealth = GetComponent<UnitHealth>();
+        healthbarPlane.localScale = new Vector3(XScale * unitHealth.health / unitHealth.maxHealth, 1, ZScale);
+        healthbarNumber.text = unitHealth.health.ToString();
+    }
+}
