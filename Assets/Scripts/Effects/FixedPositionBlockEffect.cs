2472d747a793ffc20dc776433143fe8e5fe55731
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-03-30 16:57:53 +0800

    * added Sidestep

---
+++ Assets/Scripts/Effects/FixedPositionBlockEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Effect/FixedPositionBlock")]
+public class FixedPositionBlockEffect : Effect
+{
+    public int[] blockgain;
+    public override void activate() {
+        GameObject battle = GameObject.FindGameObjectWithTag("Battle");
+        PlayerController pcon = battle.GetComponent<PlayerController>();
+        for(int i = 0; i < blockgain.Length; i++) {
+            pcon.block[i] += pcon.getModifiedBlock(blockgain[i]);
+        }
+    }
+}
