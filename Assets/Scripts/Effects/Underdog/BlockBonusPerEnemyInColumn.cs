da3dd1a7b29be886ffc0c653ef54ceed13a0a211
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-04-13 21:06:37 +0800

    * added Preemptive Dodge

--- Assets/Scripts/Effects/Underdog/BlockBonusPerEnemyInColumn.cs
+++ Assets/Scripts/Effects/Underdog/BlockBonusPerEnemyInColumn.cs
@@ -3,31 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu(menuName = "Effect/BonusBlockPerCardPlayed")]
-public class BlockBonusPerCardPlayed : Effect
+[CreateAssetMenu(menuName = "Effect/BonusBlockPerEnemyInColumn")]
+public class BlockBonusPerEnemyInColumn : Effect
 {
     public int initialBlock;
     public int bonusBlock;
-    public Card.CardType bonusCardType;
     public override void activate() {
         GameObject battle = GameObject.FindGameObjectWithTag("Battle");
         PlayerController pcon = battle.GetComponent<PlayerController>();
         int column = pcon.cellSelected[0];
 
-        int blockgain = getTotalBlockGain();
+        int blockgain = getTotalBlockGain(column);
         int modifiedBlockGain = pcon.getModifiedBlock(blockgain);
         pcon.block[column] += modifiedBlockGain;
 
     }
 
-    public int getTotalBlockGain() {
+    public int getTotalBlockGain(int column) {
         GameObject battle = GameObject.FindGameObjectWithTag("Battle");
-        PlayerController pcon = battle.GetComponent<PlayerController>();
+        EnemySquad esquad = battle.GetComponent<EnemySquad>();
 
         int blockgain = initialBlock;
-        foreach(Card card in pcon.turnHistory) {
-            if (card.type == bonusCardType) {
-                blockgain += bonusBlock;
+        foreach(GameObject enemy in esquad.enemies) {
+            GridPosition gpos = enemy.GetComponent<GridPosition>();
+            if(gpos != null) {
+                if (gpos.column == column) {
+                    blockgain += bonusBlock;
+                }
             }
         }
         return Mathf.Clamp(blockgain, 0, blockgain + 1);
