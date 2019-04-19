0d895bb8a656f250dd20d54c91cf9a1c83ea5e3e
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-04-19 21:18:38 +0800

    * added Player Block UI

---
+++ Assets/Scripts/UI/PlayerBlockUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlockUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] blockShield;
+    public TextMesh[] blockAmount;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateBlock();
+    }
+
+    void updateBlock() {
+        PlayerController pcon = GameObject.FindGameObjectWithTag("Battle").GetComponent<PlayerController>();
+        for(int i = 0; i < 4; i++) {
+            if(pcon.block[i] <= 0) {
+                blockShield[i].SetActive(false);
+            } else {
+                blockShield[i].SetActive(true);
+            }
+            blockAmount[i].text = pcon.block[i].ToString();
+        }
+
+    }
+}
