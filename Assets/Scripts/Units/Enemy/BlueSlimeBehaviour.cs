53db6bdb93959997c656db76c8a41037e16d7590
ghosalya <12974269+ghosalya@users.noreply.github.com>
2019-04-19 15:47:13 +0800

    * added Blue Slime * added UI for enemy block

---
+++ Assets/Scripts/Units/Enemy/BlueSlimeBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueSlimeBehaviour : UnitBehaviour
+{
+    public int meleeDamage;
+    public int blockGain;
+    public ParticleSystem attackAnimation;
+    int turnCounter = 0;
+    public override void act() {
+        if (turnCounter == 0) {
+            moveOrAttack();
+        } else if (turnCounter == 1) {
+            UnitHealth health = GetComponent<UnitHealth>();
+            health.gainBlock(blockGain);
+        }
+
+        turnCounter = (turnCounter + 1) % 2;
+    }
+
+    void moveOrAttack() {
+        GridPosition gridpos = GetComponent<GridPosition>();
+        if (gridpos.row > 1) {
+            gridpos.row -= 1;
+        } else {
+            dealMeleeDamage(gridpos.column);
+        }
+    }
+
+
+    void dealMeleeDamage(int column) {
+        // damage player
+        GameObject battle = GameObject.FindGameObjectWithTag("Battle");
+        PlayerController pcon = battle.GetComponent<PlayerController>();
+        pcon.takeDamage(meleeDamage, column);
+        attackAnimation.Play();
+    }
+
+
+}
